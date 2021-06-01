63f0f7dfef6fd53a153ba27759f8883d372c5ff0
lindaoktaviyani <oktaviyani.linda@gmail.com>
2021-06-01 23:31:19 +0800

    ouput dengan messagedialog

---
+++ modul1/DataTypes3.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class DataTypes3{
+ public static void main(String [] args){
+
+ String namaDepan = "Linda";
+ String namaBelakang = "Oktaviyani";
+ int usia = 19;
+ int targetTahunKuliah = 4;
+ double ipk = 3.89764512;
+ char nilaiAbjad = 'A';
+ boolean cantik = true;
+ 
+ System.out.println("Nama Depan : "+ namaDepan);
+ System.out.println("Nama Belakang : "+ namaBelakang);
+ System.out.println("Usia : "+usia);
+ System.out.println("Targer kuliah : "+ targetTahunKuliah + " tahun");
+ System.out.println("IPK : "+ ipk); 
+ System.out.println("Nilai PBO : "+ nilaiAbjad);
+ System.out.println("Cantik : "+ cantik);
+
+ JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaBelakang);
+ }
+}

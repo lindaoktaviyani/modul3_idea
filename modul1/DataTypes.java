7e356670375bd73d1f0e9518995cb4f7be0c590b
lindaoktaviyani <oktaviyani.linda@gmail.com>
2021-06-01 23:13:14 +0800

    deklarasi variabel dan tipe data

---
+++ modul1/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String [] args){
+
+ String namaDepan = "Linda";
+ String namaBelakang = "Oktaviyani";
+ int usia = 19;
+ int targetTahunKuliah = 4;
+ double ipk = 3.89764512;
+ char nilaiAbjad = 'A';
+ boolean cantik = true;
+ 
+ }
+}

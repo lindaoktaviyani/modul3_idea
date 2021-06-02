eb044f61d4e0e61712420393f47169912eb1139b
lindaoktaviyani <oktaviyani.linda@gmail.com>
2021-06-02 17:22:47 +0800

    Project IDEA

--- modul1/modul_idea/src/Main.java
+++ modul1/modul_idea/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         String namaDepan = "Linda";
         String namaBelakang = "Oktaviyani";
         int usia = 19;
-        int targetTahunKuliaj = 4;
+        int targetTahunKuliah = 4;
         double ipk = 3.89764512;
         char nilaiAbjad = 'A';
         boolean cantik = true;

bc7313bafa7f8f0275ad5e1df90ec37ce34578d8
lindaoktaviyani <oktaviyani.linda@gmail.com>
2021-06-09 16:56:43 +0800

    Operasi tipe String

--- modul1/modul_idea/src/Main2.java
+++ modul1/modul_idea/src/Main2.java
@@ -13,5 +13,21 @@ public class Main2 {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("Lin", "Barr");
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split("");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }

3d33427f3ad09a3c930af86871c4f3e81ad996db
lindaoktaviyani <oktaviyani.linda@gmail.com>
2021-06-14 20:10:09 +0800

    java GUI

--- modul1/modul3_idea/src/GuiByText.java
+++ modul1/modul3_idea/src/GuiByText.java
@@ -1,12 +1,42 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame{
+    JLabel label1 = new JLabel("Input Nama");
+    JTextField textField1 = new JTextField();
+    JButton button1 = new JButton("Greet");
+
     GuiByText(){
         setSize(640, 480);
+        setLocationRelativeTo(null);
+        setTitle("GUI By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    void meletakkanKomponen(){
+        getContentPane().setLayout(null);
+
+        getContentPane().add(label1);
+        getContentPane().add(textField1);
+        getContentPane().add(button1);
+
+        label1.setBounds(16,32,100,25);
+        textField1.setBounds(124,32,100,25);
+        button1.setBounds(232,32,80,25);
+    }
+
+    void meletakkanlistener(){
+        button1.addActionListener(e -> {
+            String nama = textField1.getText();
+            JOptionPane.showMessageDialog(null,"Hai " + nama);
+        });
     }
 
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
+        guiByText.meletakkanlistener();
         guiByText.setVisible(true);
     }
 }
